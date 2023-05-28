c5d07f401ef9296ac6503ec1d6dd2b7b35c89af0
mynk1997 <mynk1997@gmail.com>
2023-05-28 23:21:07 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading= React.createElement("h1",{id:"heading"},"Hello from React");
+
+const parent= React.createElement("div",{class:"parent"},[heading,
+React.createElement("div",{class:"child1"},React.createElement("div",{class:"child"},"This is the child of 1st child"),
+React.createElement("div",{class:'child2'},"this is the 2nd child"))])
+
+console.log(parent); //React Element is an object and its attributes are props.
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);  
